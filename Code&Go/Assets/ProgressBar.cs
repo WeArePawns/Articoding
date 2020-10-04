27731c5613895b7c77724d21a90b59b6dd6ee716
DFaouaz <dfaouaz@ucm.es>
2020-10-04 15:30:46 +0200

    Radial Progress Bar started

---
+++ Code&Go/Assets/ProgressBar.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode()]
+public class ProgressBar : MonoBehaviour
+{
+    public int minimum;
+    public int maximum;
+    public int current;
+    public Color fillColor;
+    public Image mask;
+    public Image fill;
+
+    private void Update()
+    {
+        GetCurrentFill();
+    }
+
+    private void GetCurrentFill()
+    {
+        float currentOffset = current - minimum;
+        float maximumOffset = maximum - minimum;
+        float fillRatio = currentOffset / maximumOffset;
+        mask.fillAmount = fillRatio;
+
+        fill.color = fillColor;
+    }
+}
