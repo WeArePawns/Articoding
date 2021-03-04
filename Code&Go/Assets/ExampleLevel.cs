ed99a458fdae1bf98a4fbc72dcd0fd3cfbc21af6
DFaouaz <dfaouaz@ucm.es>
2021-03-04 17:52:30 +0100

    Preparado nivel de ejemplo

---
+++ Code&Go/Assets/ExampleLevel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// TODO: Interfaz ILevel generico para todo nivel
+/*
+ * Condicion de complecion
+ * OnLevelFinished(), ...
+ * etc
+ */
+
+public class ExampleLevel : Level
+{
+    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject goal;
+
+    public override bool IsCompleted()
+    {
+        return Mathf.Abs(player.transform.position.x - goal.transform.position.x) <= Mathf.Epsilon;
+    }
+
+    public override void OnLevelCompleted()
+    {
+        Debug.Log("Level completed");
+    }
+
+    public override void OnLevelStarted()
+    {
+        Debug.Log("Level started");
+    }
+}
