ad3564fd75ab9a8b7d6fd9d3668b10263929cc59
DFaouaz <dfaouaz@ucm.es>
2021-03-18 00:04:32 +0100

    Panel de Bloques colocado
    También se ha hecho un script pequeño para abrir y cerrar los paneles de temario y enunciado

---
+++ Code&Go/Assets/LevelUIController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelUIController : MonoBehaviour
+{
+    [SerializeField] private RectTransform temaryPanel;
+    [SerializeField] private RectTransform sentencePanel;
+
+    void Start()
+    {
+        temaryPanel.gameObject.SetActive(false);
+        sentencePanel.gameObject.SetActive(false);
+    }
+
+
+    public void TriggerPanel(RectTransform panel)
+    {
+        RectTransform other = panel != temaryPanel ? temaryPanel : sentencePanel;
+
+        if(panel.gameObject.activeSelf)
+        {
+            ClosePanel(panel);
+        }
+        else
+        {
+            OpenPanel(panel);
+            ClosePanel(other);
+        }
+    }
+
+    public void OpenPanel(RectTransform panel)
+    {
+        panel.gameObject.SetActive(true);
+    }
+    
+    public void ClosePanel(RectTransform panel)
+    {
+        panel.gameObject.SetActive(false);
+    }
+
+}
