d0a72d11e945163d94698aa52905b8ba18ccfcce
argarc03 <argarc03@ucm.es>
2021-03-20 00:31:10 +0100

    Footer, SettingsMenu y ExitConfirmationMenu

---
+++ Code&Go/Assets/Scripts/MenusManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenusManager : MonoBehaviour
+{
+    public GameObject blackPanel;
+    public GameObject exitConfirmationPanel;
+    public GameObject settingsMenu;
+
+
+
+    void Start()
+    {
+        exitConfirmationPanel.SetActive(false);
+        blackPanel.SetActive(false);
+        settingsMenu.SetActive(false);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void toggleSettingsMenu()
+    {
+        settingsMenu.SetActive(!settingsMenu.activeSelf);
+    }
+
+    public void setActiveExitConfirmationPanel(bool active)
+    {
+        exitConfirmationPanel.SetActive(active);
+        blackPanel.SetActive(active);
+    }
+
+    public void exitGame()
+    {
+        //GameManager.instance
+        Application.Quit();
+    }
+}
