a7a79024d8c45f1e511c44a8bf9801a00839815a
DFaouaz <dfaouaz@ucm.es>
2021-03-27 13:16:17 +0100

    BoardManager emepezado

---
+++ Code&Go/Assets/Scripts/Board/BoardCell.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardCell : MonoBehaviour
+{
+    private int x;
+    private int y;
+    private GameObject placedObject;
+
+    public void SetPosition(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+        transform.position = new Vector2(x, y);
+    }
+
+    public void SetPosition(Vector2Int position)
+    {
+        x = position.x;
+        y = position.y;
+        transform.position = new Vector2(x, y);
+    }
+
+    public Vector2Int GetPosition()
+    {
+        return new Vector2Int(x, y);
+    }
+
+    public bool PlaceObject(GameObject gameObject)
+    {
+        if (placedObject != null) return false; // Cannot placed, cell ocuppied
+
+        placedObject = gameObject;
+        return true;
+    }
+}
---
+++ Code&Go/Assets/Scripts/Board/BoardManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour
+{
+    public int rows;
+    public int columns;
+
+    public BoardCell cellPrefab;
+
+    private void Awake()
+    {
+        GenerateBoard();
+    }
+
+    // Current gameObject is parent of the cells
+    private void GenerateBoard()
+    {
+        // If a board already exist, destroy it
+        DestroyBoard();
+
+        // Initialize board
+        for(int y = 0; y < rows; y++)
+        {
+            for(int x = 0; x < columns; x++)
+            {
+                BoardCell cell = Instantiate(cellPrefab, transform);
+                cell.SetPosition(x, y);
+            }
+        }
+    }
+
+    private void DestroyBoard()
+    {
+        foreach (Transform child in transform)
+        {
+            Destroy(child.gameObject);
+        }
+    }
+
+}
