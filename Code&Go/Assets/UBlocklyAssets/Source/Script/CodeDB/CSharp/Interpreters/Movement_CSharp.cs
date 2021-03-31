3ccf2a00a9c86ab48f61605321b885be96085732
apoyatos <apoyatos@ucm.es>
2021-03-31 19:52:30 +0200

    Bloques del mundo de variables creados
    -Creada la estructura de los bloques del mundo de variables (mover, girar y cambiar intensidad)
    TODO:
    -Implementar la funcionalidad de los bloques

---
+++ Code&Go/Assets/UBlocklyAssets/Source/Script/CodeDB/CSharp/Interpreters/Movement_CSharp.cs
@@ -0,0 +1,90 @@
+﻿/****************************************************************************
+
+Functions for interpreting c# code for blocks.
+
+Copyright 2016 sophieml1989@gmail.com
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+****************************************************************************/
+
+
+using System.Collections;
+using UnityEngine;
+
+namespace UBlockly
+{
+    [CodeInterpreter(BlockType = "movement_move")]
+    public class Move_Cmdtor : EnumeratorCmdtor
+    {
+        protected override IEnumerator Execute(Block block)
+        {
+
+            //CustomEnumerator ctor = CSharp.Interpreter.ValueReturn(block, "AMOUNT", new DataStruct(0));
+            //yield return ctor;
+            //DataStruct arg0 = ctor.Data;
+            //arg0.NumberValue;
+
+            //string op = block.GetFieldValue("DIRECTION");
+            //switch (op)
+            //{
+            //    case "UP":
+            //        break;
+            //    case "DOWN":
+            //        break;
+            //    case "LEFT":
+            //        break;
+            //    case "RIGHT":
+            //        break;
+            //}
+
+            yield return null;
+        }
+    }
+
+    [CodeInterpreter(BlockType = "movement_rotate")]
+    public class Rotate_Cmdtor : EnumeratorCmdtor
+    {
+        protected override IEnumerator Execute(Block block)
+        {
+            //    CustomEnumerator ctor = CSharp.Interpreter.ValueReturn(block, "AMOUNT", new DataStruct(0));
+            //    yield return ctor;
+            //    DataStruct arg0 = ctor.Data;
+            //    arg0.NumberValue;
+
+            //    string op = block.GetFieldValue("ROTATION");
+            //    switch (op)
+            //    {
+            //        case "LEFT":
+            //            break;
+            //        case "RIGHT":
+            //            break;
+            //    }
+            yield return null;
+        }
+    }
+
+    [CodeInterpreter(BlockType = "movement_change_intensity")]
+    public class Change_Intensity_Cmdtor : EnumeratorCmdtor
+    {
+        protected override IEnumerator Execute(Block block)
+        {
+            //    CustomEnumerator ctor = CSharp.Interpreter.ValueReturn(block, "AMOUNT", new DataStruct(0));
+            //    yield return ctor;
+            //    DataStruct arg0 = ctor.Data;
+            //    arg0.NumberValue;
+          
+            yield return null;
+        }
+    }
+}
