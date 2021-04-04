f2fad0daba1e83d16c26c6d31166f585318de998
DFaouaz <dfaouaz@ucm.es>
2021-04-04 12:44:21 +0200

    Sistema de tutoriales empezado
    Se ha creado un componente que va por prioridades. Si el sistema detecta que existen varios componentes de ese tipo, lo saca por prioridades.
    TODO: ampliar Holders para que puedan recibir un evento bool que se active cuando sea true.

---
+++ Code&Go/Assets/Scripts/TutotialInfoHolder.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutotialInfoHolder : MonoBehaviour, IComparable<TutotialInfoHolder>
+{
+    public int priority;
+    public TutorialInfo info;
+
+    private RectTransform mRectTransform;
+    private Renderer mRenderer;
+    private Collider mCollider;
+
+    private void Awake()
+    {
+        if ((mRectTransform = GetComponent<RectTransform>()) != null) return;
+        if ((mRenderer = GetComponent<Renderer>()) != null) return;
+        if ((mCollider = GetComponent<Collider>()) != null) return;
+    }
+
+    public Rect GetRect()
+    {
+        if (mRectTransform != null) return RectUtils.RectTransformToScreenSpace(mRectTransform);
+        if (mRenderer != null) return RectUtils.RendererToScreenSpace(mRenderer);
+        if (mCollider != null) return RectUtils.ColliderToScreenSpace(mCollider);
+        return new Rect(Screen.width / 2.0f, Screen.height / 2.0f, 0.0f, 0.0f); ;
+    }
+
+    public int CompareTo(TutotialInfoHolder other)
+    {
+        return priority - other.priority;
+    }
+}
--- Code&Go/Assets/TutorialManager.cs
+++ Code&Go/Assets/TutorialManager.cs
@@ -2,13 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class TutorialManager : MonoBehaviour
 {
-    public static TutorialManager Instance;
+    [SerializeField] PopUpManager popUpManager;
+   // public static TutorialManager Instance;
+    private BinaryHeap<TutotialInfoHolder> holders;
 
     private void Awake()
     {
+        holders = new BinaryHeap<TutotialInfoHolder>();
 
+        TutotialInfoHolder[] aux = FindObjectsOfType<TutotialInfoHolder>();
+        for(int i=0; i < aux.Length; i++)
+        {
+            holders.Add(aux[i]);
+        }
     }
-    
+
+    private void Update()
+    {
+        TryPopInitialHolders();
+    }
+
+
+    private void TryPopInitialHolders()
+    {
+        if (holders.Count == 0) return;
+        if (popUpManager == null) return;
+        if (popUpManager.IsShowing()) return;
+
+        TutotialInfoHolder infoHolder = holders.Remove();
+        TutorialInfo info = infoHolder.info;
+
+        popUpManager.Show(info.popUpData, infoHolder.GetRect());
+    }
+
 }
