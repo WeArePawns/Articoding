9875ed59b12cc46ff2269f3420aade6214034865
DFaouaz <dfaouaz@ucm.es>
2021-04-07 19:42:18 +0200

    Estructura de carpetas corregida

--- Code&Go/Assets/Scripts/Board/Objects/BasicMirror.cs
+++ Code&Go/Assets/Scripts/Board/Objects/BasicMirror.cs
@@ -13,7 +13,7 @@ public class BasicMirror : BoardObject, ILaserReflector
 
     public void Reflect(Vector3 inFrom, Vector3 inDirection, Vector3 inNormal, out Vector3[] outFrom, out Vector3[] outDirection)
     {
-        if (Vector3.Dot(inFrom, reflectCollider.gameObject.transform.right.normalized) <= 0.0) 
+        if (Mathf.Approximately(1.0f, Vector3.Dot(inNormal, reflectCollider.gameObject.transform.right.normalized))) 
         {
             outFrom = new Vector3[0];
             outDirection = new Vector3[0];
